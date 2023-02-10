bce895ab5ec1a5c83c4574bbbeaeab076319cc00
Alex Calderwood <alexander.d.calderwood@gmail.com>
2023-02-10 15:09:15 -0800

    Wrote a Step REPL for faster testing.
    I had to rename the folder also. There is now a way to execute step commands in a command line-like environment with the new interpreter.

---
+++ StoryAssemblerRedux/Step/StepDrivers/StepREPL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Step;
+
+namespace StepDrivers
+{
+    internal class StepREPL
+    {
+
+        static void Main(string[] args)
+            {
+                Module module = new Module("Run");
+                Console.WriteLine(module);
+
+                module.LoadDirectory("\\\\Mac\\code\\retl\\RCR\\StoryAssemblerRedux\\StoryAssembler\\");
+                Console.WriteLine("Loaded");
+
+                while (true)
+                        {
+                            Console.WriteLine("Enter Step code or type 'q' to exit:");
+                            string input = Console.ReadLine();
+                            if (input.ToLower() == "q")
+                            {
+                                break;
+                            }
+
+                            try
+                            {
+                                ExecuteStep(input, module);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("An error occurred: " + ex.Message);
+                            }
+                        }
+                    }
+
+            static void ExecuteStep(string input, Module module)
+            {
+                var result = module.ParseAndExecute(input);
+                    Console.WriteLine("Result");
+                    Console.WriteLine(result);
+                    Console.ReadLine();
+            }
+    }
+
+}
